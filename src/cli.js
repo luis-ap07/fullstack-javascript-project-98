bf4f6c908b4708b91296aede1866f1452392f775
Luis <luigi_12_6@hotmail.com>
2024-12-24 12:05:17 -0500

    Project-brain-games

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@
 import readlineSync from 'readline-sync';
 
 export const greetUser = () => {
+	console.log('Welcome to the Brain Games!');
 	const name =readlineSync.question('¿Cuál es tu nombre? ');
 	console.log(`¡Hola, ${name}!`);
 	};
