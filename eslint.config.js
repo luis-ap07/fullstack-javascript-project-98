0db0d3e19bad54702576a18402d0fdb1538c290b
Luis <luigi_12_6@hotmail.com>
2024-12-29 20:40:23 -0500

    Project-brain-games

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
 import globals from 'globals';
-import * as tseslint from '@typescript-eslint/eslint-plugin';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
-/** @type { import('eslint').Linter.Config[] } */
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
