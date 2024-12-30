57602277d098cf45437873537a61350a5081e6fc
Luis <luigi_12_6@hotmail.com>
2024-12-29 22:59:55 -0500

    Project-brain-games

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from '@typescript-eslint';
+import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 /** @type { import('eslint').Linter.Config[] } */
